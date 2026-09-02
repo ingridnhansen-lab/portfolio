f7657437def89869d46ef61b621401ada8c04c46
Ingrid Hansen <ingrid.nhansen@gmail.com>
2026-09-02 15:26:11 -0300

    Update header.tsx
    creación del componente header

--- components/header.tsx
+++ components/header.tsx
@@ -1,1 +1,20 @@
+// components/Header.tsx
+'use client'
 
+import Link from 'next/link'
+
+export function Header() {
+  return (
+    <header className="site-header">
+      <a className="wordmark" href="/">
+        Ingrid Hansen
+      </a>
+      <nav>
+        <a className="nav-active" href="/#home">Home</a>
+        <a href="/#work">Work</a>
+        <a href="/#about">About</a>
+        <a href="/#contact">Contact</a>
+      </nav>
+    </header>
+  )
+}
